a83f6d7535f89e48cedac278eb4c3283af4e5c5d
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-20 06:20:03 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,3 +8,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+def move(array, index, value)
+  array[index] = value
+  return array
+end
